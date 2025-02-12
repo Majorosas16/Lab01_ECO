ae532402d948290cc698dbdd18fc4e2aa60821e4
Maria Jose Rosas <mariajoserosas@192.168.1.5>
2025-02-11 22:55:10 -0500

    finalJS

--- index.js
+++ index.js
@@ -1,4 +1,73 @@
-// 
+const containerNobel = document.getElementById("nobel-container");
+const containerEmoji = document.getElementById("emoji-container");
+
+// API NOBEL PRIZE
+
+const buttonNobel = document.getElementById("nobel-button").addEventListener("click", fetchDataNobel);
+
+async function fetchDataNobel() {
+    renderLoadingState();
+    try {
+      const response = await fetch("https://randomuser.me/api/");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+    const data = await response.json();
+    renderNobelPrize(data);
+      
+    } catch (error) {
+      renderErrorState();
+    }
+  }
+
+  const renderNobelPrize = (data) => {
+    containerNobel.innerHTML = ""; // Clear previous data
+
+    if (!data.results || data.results.length === 0) {
+        containerNobel.innerHTML = "<p>No data available</p>";
+        return;
+    }
+
+    const txt = document.createElement("p");
+    const img = document.createElement("img");
+    img.src=`${data.results[0].picture.large}`;
+    img.alt=`${data.results[0].name.title}`;
+    txt.innerHTML = `Phone: ${data.results[0].phone}`;
+    containerNobel.appendChild(txt);
+    containerNobel.appendChild(img)
+}
+
+// API RANDOM EMOJI
+
+const emoji_button = document.getElementById("emoji-button").addEventListener("click", fetchDataEmoji);
+
+async function fetchDataEmoji() {
+    renderLoadingState();
+    try {
+      const response = await fetch("https://emojihub.yurace.pro/api/random/category/food-and-drink");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+    const data = await response.json();
+    renderEmoji(data);
+      
+    } catch (error) {
+      renderErrorState();
+    }
+  }
+
+ function renderEmoji (data) {
+    containerEmoji.innerHTML = ""; // Clear previous data
+
+    if (!data.name || data.name.length === 0) {
+      containerEmoji.innerHTML = "<p>No data available</p>";
+        return;
+    }
+
+    const txt = document.createElement("p");
+    txt.innerHTML = `${data.name}`;
+    containerEmoji.appendChild(txt);
+}
 
 // API ANIME
 document.getElementById("fetch-button").addEventListener("click", fetchData);
@@ -18,7 +87,7 @@ async function fetchData() {
     try {
         const response = await fetch(`https://api.jikan.moe/v4/anime?q=${input_raiting}&q=${input_status}&q=${input_type}`);
         if (!response.ok) {
-            throw new Error("El api no está bien");
+            throw new Error("Network response was not ok");
         }
         const data = await response.json();
         renderData(data)
@@ -30,20 +99,18 @@ async function fetchData() {
 function renderErrorState() {
     const container = document.getElementById("anime-container");
     container.innerHTML = "";
-    container.innerHTML = "<p>Error al cargar la data</p>";
-    console.log("Error al cargar el contenido");
+    container.innerHTML = "<p>Ops!, something happened</p>";
 }
 
 function renderLoadingState() {
     const container = document.getElementById("anime-container");
     container.innerHTML = ""; 
-    container.innerHTML = "<p>Cargando...</p>";
-    console.log("Cargando...");
+    container.innerHTML = "<p>Loading...</p>";
 }
 
 function renderData(animeData) {
     const container = document.getElementById("anime-container");
-    container.innerHTML = ""; // Clear previous data
+    container.innerHTML = "";
 
     animeData.data.forEach((anime) => {
       const card = document.createElement("div");
